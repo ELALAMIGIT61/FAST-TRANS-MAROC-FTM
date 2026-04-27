c318a923b0186b9168f7faa91aeb6d75b9b24b5c
ELALAMIGIT61 <Elalami19610101@gmail.com>
2026-04-27 18:25:59 +0000

    feat: add DriverOnboardingStack/DriverPendingStack routes + install expo-image-picker expo-document-picker

--- frontend/src/types/database.ts
+++ frontend/src/types/database.ts
@@ -40,5 +40,7 @@ export type AppRoute =
   | 'ProfileSetupScreen'
   | 'AccountSuspendedScreen'
   | 'ClientHomeStack'
+  | 'DriverOnboardingStack'
+  | 'DriverPendingStack'
   | 'DriverHomeStack'
   | 'AdminStack';
